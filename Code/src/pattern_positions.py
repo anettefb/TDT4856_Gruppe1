7d1cd8c52e155196cd8b8f5956632f1b7fb8b7e4
Anette Fagerheim Bjerke <anettefbjerke@anettes-macbook-air.home>
2021-03-03 11:37:32 +0100

    Updated patternposition to include depth calculations

--- Code/src/pattern_positions.py
+++ Code/src/pattern_positions.py
@@ -1,8 +1,7 @@
 def pattern_positions(pattern_dictionary):
     """ Will find the position of the different patterns and return a list
 with the angles (keys) where the pattern is. It also returns the starting 
-edges of the pattern for calculating the depth. 
-
+edges of the pattern for calculating the depth.
 """
     count = 0
     threshold = 1.8
@@ -10,9 +9,12 @@ edges of the pattern for calculating the depth.
 
     prev_key = next(iter(pattern_dictionary))
     main_key = next(iter(pattern_dictionary))
+    
+    d = 0
 
     edges = []
     angles = []
+    depth_pattern = []
 
 
     for key in pattern_dictionary:
@@ -25,16 +27,28 @@ edges of the pattern for calculating the depth.
                 angles.append(prev_key)
                 angles.append(key)
                 edges.append(main_key)
+                d += pattern_dictionary[prev_key]
+                d += pattern_dictionary[key]
 
             if count > pattern_threshold:
                 angles.append(key)
+                d += pattern_dictionary[key]
+            print(d)
+                
 
         else:
+            if prev_key in angles:
+                c = pattern_dictionary[main_key] - d/count
+                depth_pattern.append(c)
+                d = 0
+            
             main_key = key
             count = 0
+            
+            
 
         prev_key = key
 
     #print(angles)
     #print(edges)
-    return angles, edges
+    return angles, edges, depth_pattern
