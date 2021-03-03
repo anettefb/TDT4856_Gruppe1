773914bdfafe2d7fa75982f8a996158c75ce438e
Anette Fagerheim Bjerke <anettefbjerke@anettes-macbook-air.home>
2021-03-03 11:59:20 +0100

    updated with check_pattern

--- Code/src/logic.py
+++ Code/src/logic.py
@@ -21,26 +21,27 @@ dict_wheel = { 74: 500,
 92: 498,
 93: 500,
 }
-    
+
+# General information
 DATA_START = 80.0
 DATA_END = 100.0
 UNIT = "mm"
+midangle = (DATA_START-DATA_END)/2
+WINTER_THRESHOLD = 5 #mm
+SUMMER_THRESHOLD = 1.6 #mm
 
+
+#run functions
 pattern, start_edges, pattern_depth = pattern_positions(dict_wheel) # finding the postitions for mønsteret
 
 print(pattern_depth)
 print()
+ver = check_pattern(pattern_depth)
 
-"""
-def calculate_pattern_depth(start_key, pattern_key, dictionary):
-    
-    depth = []
-    
-    for key in dictionary:
+print(ver)
+
+
         
-        for item in pattern_key:
-            
-"""
 
 
 
