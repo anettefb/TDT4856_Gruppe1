69e3ecccd57f1f234563e704f00ecb7d7f609225
Anette Fagerheim Bjerke <anettefbjerke@anettes-macbook-air.home>
2021-03-03 15:25:35 +0100

    :)

--- Code/src/check_pattern.py
+++
@@ -1,9 +0,0 @@
-def check_pattern(depth):
-    threshold = 5
-    approved = []
-    for item in depth:
-        if item > threshold:
-            approved.append("Yes")
-        else:
-            approved.append("No")
-    return approved
