fd4944ea21e926c9cca56288637c62a186ae7299
Anette Fagerheim Bjerke <anettefbjerke@anettes-macbook-air.home>
2021-03-03 13:38:10 +0100

    Brunch

--- Code/src/files.py
+++ Code/src/files.py
@@ -1,1 +1,49 @@
-print("Hello World")
+import sys
+import os
+import getopt
+import re
+
+
+_inputfile_path = ""
+measurements = {}
+
+def file_handling():
+    parse(sys.argv[1:])
+    load()
+
+def help():
+    print("This is a help message. Run the program as follows:")
+    print("python(3) files.py -i <inputfile> [-h]")
+
+def parse(argv):
+    global _inputfile_path
+    try:
+        opts, args = getopt.getopt(argv, "hi:", ["ifile"])
+    except getopt.GetoptError:
+        help()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            help()
+        elif opt in ("-i", "--ifile"):
+            _inputfile_path = arg
+        else:
+            help()
+            sys.exit(2)
+    if not os.path.exists(_inputfile_path):
+        help()
+        sys.exit(2)
+    print("Path is valid: " + str(_inputfile_path) + '\n')
+
+def load():
+    global measurements
+    with open(_inputfile_path, 'r') as file:
+        for line in file:
+            list_line = (line.rstrip()).split(' ')
+            if len(list_line) < 2:
+                continue
+            elif re.match(r'^-?\d+(?:\.\d+)$', list_line[0]) is None:
+                continue
+            angule = float(list_line[0])
+            distance = float(list_line[1])
+            measurements[angule] = distance
