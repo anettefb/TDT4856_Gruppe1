d7a9819b3adca6ae0878d6686ef7efd8b11a9a0d
Anette Fagerheim Bjerke <anettefbjerke@anettes-macbook-air.home>
2021-03-03 11:38:37 +0100

    Updated logic based on addition in pattern_position

--- Code/src/logic.py
+++ Code/src/logic.py
@@ -1,6 +1,4 @@
 from pattern_positions import pattern_positions
-        
-
 
 dict_wheel = { 74: 500,
 75: 496,
@@ -20,16 +18,29 @@ dict_wheel = { 74: 500,
 89: 500,
 90: 498,
 91: 498,
+92: 498,
+93: 500,
 }
     
 DATA_START = 80.0
 DATA_END = 100.0
 UNIT = "mm"
 
-pattern, edges = pattern_positions(dict_wheel) # finding the postitions for mønsteret
+pattern, start_edges, pattern_depth = pattern_positions(dict_wheel) # finding the postitions for mønsteret
 
-print(pattern)
-print(edges)
+print(pattern_depth)
+print()
+
+"""
+def calculate_pattern_depth(start_key, pattern_key, dictionary):
+    
+    depth = []
+    
+    for key in dictionary:
+        
+        for item in pattern_key:
+            
+"""
 
 
 
